4faf4a11dce404901d904d3ef0ab79faa2ef302a
Salma Torres <tsalma569@gmail.com>
2024-05-27 23:25:40 -0400

    Movemos el método getEmployeesByBirthDate desde BirthdarService a EmployeesRepository

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -1,34 +1,10 @@
-import fs from "fs";
-import path from "path";
-import { Employee } from "./Employee";
+import { EmployeesRepository } from "./EmployeesRepository";
 
 export class BirthdayService {
   constructor() {}
 
   sendGreetings(ourDate, fileName, smtpUrl, smtpPort, transport) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
-      "UTF-8"
-    );
-
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-    const employees = lines
-      .map((line) => this.createEmployeeFromLine(line))
-      .filter((employee) => employee.isBirthday(ourDate));
-
-    employees.forEach((employee) => {
-      const message = {
-        host: smtpUrl,
-        port: smtpPort,
-        from: "sender@here.com",
-        to: [employee.getEmail()],
-        subject: "Happy Birthday!",
-        text: `Happy Birthday, dear ${employee.getFirstName()}!`,
-      };
-      transport.sendMail(message);
-    });
+    
   }
 
   createEmployeeFromLine(line) {
--- src/birthday/EmployeesRepository.js
+++ src/birthday/EmployeesRepository.js
@@ -1,5 +1,35 @@
+import fs from "fs";
+import path from "path";
+import { Employee } from "./Employee";
+
 export class EmployeesRepository{
     constructor(fileName){
         this.fileName = fileName;
     };
+
+    getEmployesByBirthday(ourDate){
+        const data = fs.readFileSync(
+            path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
+            "UTF-8"
+        );
+      
+        // split the contents by new line
+        const lines = data.split(/\r?\n/);
+        lines.shift();
+        const employees = lines
+            .map((line) => this.createEmployeeFromLine(line))
+            .filter((employee) => employee.isBirthday(ourDate));
+      
+        employees.forEach((employee) => {
+            const message = {
+                host: smtpUrl,
+                port: smtpPort,
+                from: "sender@here.com",
+                to: [employee.getEmail()],
+                subject: "Happy Birthday!",
+                text: `Happy Birthday, dear ${employee.getFirstName()}!`,
+            };
+            transport.sendMail(message);
+          });
+    }
 }
